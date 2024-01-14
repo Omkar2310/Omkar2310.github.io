3bcf89502f2f1bc4a39e16f32c58d4208d0f2072
Omkar2310 <mankar.omkar2310@gmail.com>
2024-01-14 16:04:25 +0530

    added more projects

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -5,6 +5,8 @@ import Particle from "../Particle";
 import sangit from "../../Assets/Projects/sangit.gif";
 import todo from "../../Assets/Projects/todo.gif";
 import finance from "../../Assets/Projects/finance.png";
+import shopping from "../../Assets/Projects/shopping.webp";
+import pothole from "../../Assets/Projects/pothole.webp";
 
 function Projects() {
   return (
@@ -48,6 +50,28 @@ function Projects() {
               description="WebApp for managing the finances by adding the expenses along wiht a visual representation of each expense."
               ghLink="https://github.com/omkar2310/artha-sakshar"
               demoLink="https://om-trackartha.vercel.app"
+            />
+          </Col>
+         
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={shopping}
+              isBlog={false}
+              title="Amazon Clone App"
+              description="Built amazon clone where we can add items to the cart, place order view the items."
+              ghLink="https://github.com/Omkar2310/amazon-clone"
+              demoLink="https://clone-f2c03.firebaseapp.com/"
+            />
+          </Col>
+          
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={pothole}
+              isBlog={false}
+              title="Pothole Fixit App"
+              description="App for Pothole Detection using Deep Learning. Clicked image is detected for pothole presence
+              If pothole is present complaint forwarded to PWD department, User can track their complaints"
+              ghLink="https://github.com/Omkar2310/PotholeFixitApp"
             />
           </Col>
         </Row>
